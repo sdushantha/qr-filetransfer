b7f0b1f7220fea77ab14933475cc6737e3803bdb
Siddharth Dushantha <siddharth.dushantha@gmail.com>
2018-04-01 18:43:43 +0200

    Made some minor changes

--- qr-filetransfer.py
+++ qr-filetransfer.py
@@ -9,7 +9,7 @@ import socket
 import argparse
 import sys
 from shutil import make_archive
-
+import pathlib
 
 def get_local_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -22,41 +22,42 @@ def random_port():
 	return str(port)[5:9]
 
 
-def start_server(file):
+def start_server(fname):
     PORT = random_port()
     LOCAL_IP = get_local_ip()
     
-    if file.startswith("/"):
+    if fname.startswith("/"):
     	os.chdir("/")
 
-    if os.path.isdir(file):
-        zip_name = file.split("/")
-        zip_name = zip_name[-2]
+    if os.path.isdir(fname):
+        zip_name = pathlib.PurePosixPath(fname).name
 
         try:
-            path_to_zip = make_archive(zip_name, "zip", file)
-            file = path_to_zip.replace(os.getcwd(), "")
+            path_to_zip = make_archive(zip_name, "zip", fname)
+            fname = path_to_zip.replace(os.getcwd(), "")
+
         except PermissionError:
             print("PermissionError: Try with sudo")
-            exit()  
+            sys.exit()  
         
 
     handler = http.server.SimpleHTTPRequestHandler
 
     try:
         httpd = socketserver.TCPServer(("", int(PORT)), handler)
-    except:
+        
+    except PermissionError:
         # An error sometimes occurs randomly. 
         print("Error: Please try again")
 
-    address = "http://" + str(LOCAL_IP) + ":" + str(PORT) + "/" + file
+    address = "http://" + str(LOCAL_IP) + ":" + str(PORT) + "/" + fname
 
     print("Scan the following QR to start downloading.\nMake sure that your smartphone is connected to the same WiFi network as this computer.")
-    make_qr(address)
+    print_qr_code(address)
     httpd.serve_forever()
 
 
-def make_qr(address):
+def print_qr_code(address):
     qr = qrcode.QRCode(1)
     qr.add_data(address)
     qr.make()
@@ -64,21 +65,21 @@ def make_qr(address):
 
 
 def main():
-	parser = argparse.ArgumentParser(description = "Transfer files over wifi from your computer to your mobile device by scanning a QR code without leaving the terminal.")
+    parser = argparse.ArgumentParser(description = "Transfer files over wifi from your computer to your mobile device by scanning a QR code without leaving the terminal.")
 
-	parser.add_argument("-f", "--file",
-			type=str,
+    parser.add_argument("-f", "--file",
 			required=True,
-			help = "File to be shared")
+			help="File to be shared")
 
-	args = parser.parse_args()
+    args = parser.parse_args()
 
 
-	if len(sys.argv) == 1:
-		parser.print_help()
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
 
-	elif args.file:
-		start_server(file=args.file)
+
+    start_server(fname=args.file)
 
 
 if __name__=="__main__":
